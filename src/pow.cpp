27e56d7b9a6bdd2f21a5771d1ac1b50b1f04a46d
joeuhren <46763106+joeuhren@users.noreply.github.com>
2019-05-28 01:49:31 -0600

    remove pow block check

--- src/pow.cpp
+++ src/pow.cpp
@@ -124,10 +124,6 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > Params().ProofOfWorkLimit())
         return error("CheckProofOfWork() : nBits below minimum work");
 
-    // Check proof of work matches claimed amount
-    if (hash > bnTarget)
-        return error("CheckProofOfWork() : hash doesn't match nBits");
-
     return true;
 }
 
